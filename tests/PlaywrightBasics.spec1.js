86652f46d67c328cd7a980a916bcb7f83c9c815c
SDETechieGuy <sdettechie@gmail.com>
2025-07-18 17:20:51 +0530

    Modified testcase names

--- tests/PlaywrightBasics.spec1.js
+++ tests/PlaywrightBasics.spec1.js
@@ -1,13 +1,13 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('Page Context Testcase 2', async ({ page }) => {
+test('Page Context TC 2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   console.log("Title of Playwright website when loaded is:"+page.title);
   await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
 });
 
-test('Browser Context testcase 2', async ({ browser }) => {
+test('Browser Context TC 2', async ({ browser }) => {
 
   const context = await browser.newContext();
   const page = await context.newPage();
