c416212cc3158a9ca0923ee27d094fc674a17a1f
SDETechieGuy <sdettechie@gmail.com>
2025-07-18 17:17:48 +0530

    Added a new spec file with 2 tests

---
+++ tests/PlaywrightBasics.spec1.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Page Context Testcase 2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  console.log("Title of Playwright website when loaded is:"+page.title);
+  await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+});
+
+test('Browser Context testcase 2', async ({ browser }) => {
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.google.com/');
+  console.log("Title of Google browser when loaded is:"+await page.title);
+  await expect(page).toHaveTitle("Google");
+});
